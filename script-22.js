763bf42121af9ae28ee1bdc509492f264c1bda9f
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-06-01 19:08:27 +0800

    22 Pengkondisian if else if else

---
+++ script-22.js
@@ -0,0 +1,11 @@
+console.log("22 Pengkondisian If Else If Else");
+
+var angka = prompt("Masukkan Angka: ");
+
+if(angka % 2 === 0) {
+  alert(angka + " adalah bilangan GENAP");
+} else if(angka % 2 == 1) {
+  alert(angka + " adalah bilangan GANJIL");
+} else {
+  alert("Yang anda masukkan bukan angka!");
+}
