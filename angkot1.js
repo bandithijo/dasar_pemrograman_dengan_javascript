dc947fc2e4fd2a693728e5db33b1d879e77c6ff7
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-06-01 19:09:06 +0800

    Add tugas angkot 1 (while)

---
+++ angkot1.js
@@ -0,0 +1,8 @@
+console.log("18 Tugas Angkot");
+
+var angkot = 1
+var jumlah_angkot = 10;
+while(angkot <= jumlah_angkot) {
+  console.log(`Angkot No. ${angkot} beroperasi dengan baik!`);
+  angkot++;
+}
